d5f69178891ba5be9fdea070e23886904dedc36d
Roberto Cestari <robertotcestari@gmail.com>
2022-03-23 18:18:47 -0300

    initial code 🐶

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import scream from './scream.mp3';
+import { useState, useEffect } from 'react';
+
+const PLAYERS = [
+  'Gabriel',
+  'Wolf',
+  'Cestari',
+  'My Friend',
+  'Edu',
+  'Daniel',
+  'Bruno',
+  'André',
+];
 
 function App() {
+  const [startTimer, setStartTimer] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  const [isPlaying, setIsPlaying] = useState('');
+  const [availablePlayers, setAvailablePlayers] = useState(PLAYERS);
+
+  const getRandomPlayer = () => {
+    const randomPlayer =
+      availablePlayers[Math.floor(Math.random() * availablePlayers.length)];
+    setAvailablePlayers(
+      availablePlayers.filter((player) => player !== randomPlayer)
+    );
+    setIsPlaying(randomPlayer);
+
+    if (availablePlayers.length === 1) {
+      setAvailablePlayers(PLAYERS);
+    }
+
+    return randomPlayer;
+  };
+
+  useEffect(() => {
+    if (!startTimer) return;
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [startTimer]);
+
+  useEffect(() => {
+    if (seconds === 30 || seconds === 1) {
+      const audio = new Audio(scream);
+      audio.play();
+      console.log('alarme');
+      // if(availablePlayers.length)
+      getRandomPlayer();
+    }
+    if (seconds === 60) {
+      setSeconds(0);
+      setMinutes((minutes) => minutes + 1);
+    }
+  }, [seconds]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ position: 'relative' }}>
+          <img width={200} src={logo} alt="logo" />
+          <h1>
+            {isPlaying}
+          </h1>
+        </div>
+        <span>
+          {`${minutes}`.padStart(2, '0')}:{`${seconds}`.padStart(2, '0')}
+        </span>
+        <button onClick={() => setStartTimer(true)}>Iniciar</button>
       </header>
     </div>
   );
